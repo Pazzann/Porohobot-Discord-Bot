f4c4fcbb3faf17cda2972a33a2c662d1198d20ce
boyfromgod <manton2007@gmail.com>
2022-06-30 01:38:03 +0300

    Update DickDraw.js

--- commands/methods/DickDraw.js
+++ commands/methods/DickDraw.js
@@ -2,22 +2,17 @@
 
 module.exports.DickDraw = function (length)
 {
+        let dick = "8";
         if (length >= 0){
-            let dick = "8";
-
             for (let i = 0; i < length; i++) {
                 dick += "=";
             }
-
             return dick + "D";
         }else{
             length *= (-1);
-            let dick = "8";
-
             for (let i = 0; i < length; i++) {
                 dick += "=";
             }
-
             return dick + "D🍑";
         }
 
