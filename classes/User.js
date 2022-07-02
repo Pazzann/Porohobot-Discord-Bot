3edad62f3f1200f4a6662d6caa35c87d1f704a05
boyfromgod <manton2007@gmail.com>
2022-07-02 16:47:28 +0300

    add help, musicinfo

--- classes/User.js
+++ classes/User.js
@@ -3,6 +3,7 @@ module.exports.User = class {
     constructor(interaction) {
         this.userName = interaction.member.user.tag;
         this.userId = interaction.member.user.id;
+        this.porohobotCredits = 0;
 
         //dick game part
         this.DickGame ={
--- commands/musicommands/musicinfo.js
+++ commands/musicommands/musicinfo.js
@@ -1,12 +1,33 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
+const path = require('node:path');
+const fs = require('node:fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('musicinfo')
         .setDescription('Допомога зі списком плейлістів'),
-    async execute(interaction){
+    async execute(interaction) {
+        const filePath = path.join(__dirname);
+        const playlists = JSON.parse(fs.readFileSync(filePath + "\\../../music/playlist.json", "utf8"))
 
-        interaction.reply("IN DEVELOPEMENT");
+        let j = 0;
+        let playlistDesc = [];
+        for (let [key, values] of Object.entries(playlists)) {
+            for (let value of values) {
+                playlistDesc[j] = {
+                    name: `🎵 ${value?.playlistName}`,
+                    value: `🎶 **Опис:** ${value?.playlistDescription}\n💿 **Жанр:** ${key}`
+                }
+                j++;
+            }
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle("📣 Список музики:")
+            .setColor('#adffb9')
+            .addFields(...playlistDesc)
+
+        interaction.reply({embeds: [embed]});
     }
 }
